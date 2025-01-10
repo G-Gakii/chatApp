502d4beb016664ab06b782a870772349598c9884
“G-Gakii” <gakiiviolet1@gmail.com>
2025-01-10 19:31:36 +0300

    git ignore

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,10 +10,11 @@ export class AuthService {
   constructor(private afAuth: AngularFireAuth) {}
 
   registerUser(user: User) {
-    return this.afAuth.createUserWithEmailAndPassword(
-      user.email,
-      user.password
-    );
+    return this.afAuth
+      .createUserWithEmailAndPassword(user.email, user.password)
+      .then(() => {
+        this.sendEmailVerification();
+      });
   }
   loginUser(user: User) {
     return this.afAuth.signInWithEmailAndPassword(user.email, user.password);
@@ -28,5 +29,17 @@ export class AuthService {
 
   logInWithGoogle() {
     return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  resetPassword(email: string) {
+    return this.afAuth.sendPasswordResetEmail(email);
+  }
+  sendEmailVerification() {
+    return this.afAuth.currentUser.then((user) => {
+      if (user) {
+        user.sendEmailVerification();
+        alert('Verification Email sent');
+      }
+      throw new Error('User ');
+    });
   }
 }
